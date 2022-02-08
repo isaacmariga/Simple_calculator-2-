7fa7f2f9499d18cba2226576b172b9fc0bd1de4f
isaacmariga <isaac.wang'ombe@student.moringaschool.com>
2022-02-08 13:26:53 +0300

    Slight js edits

--- main.js
+++ main.js
@@ -3,9 +3,17 @@
 // })
 
 
-
-
 $(".num").click(function() {
-    let number = (this.id); // or alert($(this).attr('id'));
+    let number = (this.id);
     $(".ans").append(number)
+
+    $('#equal').click(function() {
+        alert('.number')
+
+    })
+
+});
+$(".symb").click(function() {
+    let symbol = (this.id);
+    $(".ans").append(symbol)
 });
